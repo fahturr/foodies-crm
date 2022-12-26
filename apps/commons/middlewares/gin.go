fb902e1437da677796df56f1e507501db2557730
fahturr <fahtur.rf@gmail.com>
2022-12-26 09:51:56 +0700

    build(init): add common middleware

--- apps/commons/middlewares/gin.go
+++ apps/commons/middlewares/gin.go
@@ -1,7 +1,36 @@
 package middlewares
 
+import (
+	"foodies-crm/apps/commons/response"
+	"foodies-crm/pkg/token"
+	"github.com/gin-gonic/gin"
+	"strings"
+)
+
 type GinMiddleware struct{}
 
 func NewGinMiddleware() *GinMiddleware {
 	return &GinMiddleware{}
+}
+
+func (m *GinMiddleware) ValidateAuth(ctx *gin.Context) {
+	auth := ctx.GetHeader("Authorization")
+	tokenString := strings.Split(auth, "Bearer ")
+
+	if len(tokenString) != 2 {
+		resp := response.UnauthorizedErrorWithAdditionalInfo("len token must be 2")
+		ctx.AbortWithStatusJSON(resp.StatusCode, resp)
+		return
+	}
+
+	tok, err := token.ValidateToken(tokenString[1])
+
+	if err != nil {
+		resp := response.UnauthorizedErrorWithAdditionalInfo(err.Error())
+		ctx.AbortWithStatusJSON(resp.StatusCode, resp)
+		return
+	}
+
+	ctx.Set("authId", tok.AuthId)
+	ctx.Next()
 }
